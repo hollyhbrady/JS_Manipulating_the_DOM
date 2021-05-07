a024faffddcbf09cc368354277d45f6e7da11609
hollyhbrady <hollyhbrady@gmail.com>
2021-05-07 19:12:06 +0100

    handleNewItemSubmit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const newItem = document.querySelector('#new-item');
+    newItem.addEventListener9('submit', handleNewItemSubmit);
 
-
-    
 })
+
+const handleNewItemSubmit = function (event) {
+    event.preventDefault();
+
+    const nativeBirdEntry = createNativeBirdEntry(event.target);
+    const nativeBird = document.querySelector('#entries');
+    nativeBird.appendChild(nativeBirdEntry);
+}
+
+const createNativeBirdEntry = function (form) {
+    
+}
